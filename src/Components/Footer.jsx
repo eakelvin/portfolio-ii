88ab39d64ebf5e2707c73ff385e035a2d2bd14e5
Kelvin Akaba <akabakelvin@gmail.com>
2023-11-05 23:16:18 +0000

    footer layout

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,21 +5,16 @@ const Footer = () => {
     <footer role="contentinfo" className="footer">
       <div className="row">
         <ul className="footer__social-links">
-          {/* <li className="footer__social-link-item">
-            <a href="" title="Link to Twitter Profile">
-              <img src="./images/twitter.svg" className="footer__social-image" alt="Twitter" />
-            </a>
-          </li> */}
           <li className="footer__social-link-item">
             <a href="https://github.com/eakelvin" title="Link to Github Profile">
               <img src="./images/github.svg" className="footer__social-image" alt="Github" />
             </a>
           </li>
-          {/* <li className="footer__social-link-item">
-            <a href="https://codepen.io/nisar_hassan" title="Link to Codepen Profile">
+          <li className="footer__social-link-item">
+            <a href="https://ea-linktree.netlify.app/" title="LinkTree Profile">
               <img src="./images/codepen.svg" className="footer__social-image" alt="Codepen" />
             </a>
-          </li> */}
+          </li>
           <li className="footer__social-link-item">
             <a href="https://www.linkedin.com/in/kelvin-akaba/" title="Link to Linkedin Profile">
               <img src="./images/linkedin.svg" className="footer__social-image" alt="Linkedin" />

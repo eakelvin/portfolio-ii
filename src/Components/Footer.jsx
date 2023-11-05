71d68a381d47804d5ff11a34e06ef98b82877610
Kelvin Akaba <akabakelvin@gmail.com>
2023-11-05 23:24:44 +0000

    footer layout

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,13 +6,13 @@ const Footer = () => {
       <div className="row">
         <ul className="footer__social-links">
           <li className="footer__social-link-item">
-            <a href="https://github.com/eakelvin" title="Link to Github Profile">
-              <img src="./images/github.svg" className="footer__social-image" alt="Github" />
+            <a href="https://ea-linktree.netlify.app/" title="LinkTree Profile">
+              <img src="./images/link.svg" className="footer__social-image" alt="Codepen" />
             </a>
           </li>
           <li className="footer__social-link-item">
-            <a href="https://ea-linktree.netlify.app/" title="LinkTree Profile">
-              <img src="./images/codepen.svg" className="footer__social-image" alt="Codepen" />
+            <a href="https://github.com/eakelvin" title="Link to Github Profile">
+              <img src="./images/github.svg" className="footer__social-image" alt="Github" />
             </a>
           </li>
           <li className="footer__social-link-item">
